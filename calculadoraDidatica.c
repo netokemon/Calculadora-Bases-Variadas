7df953974d4991bf343989de0c9df9809dd54a3a
netokemon <aguinaldoanselmo5@gmail.com>
2024-09-22 20:27:18 -0300

    ADD questao 1 inteira

--- calculadoraDidatica.c
+++ calculadoraDidatica.c
@@ -3,18 +3,24 @@
 int valorInput = 0.0;
 int valorBinario[32];
 char valorHexa[32];
+int valorOcta[32];
 
-void converterBinario(int num){
-    int contador = 0;
-    while(num > 0){
-        valorBinario[contador] = num % 2;
-        num = num/2;
-        contador++;
+void converterParaBinario(int numero) {
+    int binario[32];
+    int i = 0;
+
+
+    while (numero > 0) {
+        binario[i] = numero % 2; 
+        numero = numero / 2;
+        i++;
     }
 
-    for(int i = contador - 1; i >= 0; i--){
-        printf("Seu numero em binario eh: %d", valorBinario[i]);
+    printf("O número em binário é: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binario[j]);
     }
+    printf("\n");
 }
 
 void converterHexa(int num){
@@ -34,13 +40,70 @@ void converterHexa(int num){
     for(int i = contador-1; i >= 0; i--){
         printf("Seu numero em hexadecimal eh: %c", valorHexa[i]);
     }
+    printf("\n");
 
 }
+void converterOcta(int num){
+    int contador = 0;
+
+    while(num > 0){
+        valorOcta[contador] = num % 8;
+        num = num / 8;
+        contador++;
+    }
+
+    for(int i = contador-1; i >= 0; i--){
+        printf("Seu numero em base 8 eh: %d", valorOcta[i]);
+    }
+    printf("\n");
+
+
+}
+
+void converterParaBCD(int numero) {
+    int digito;
+
+    printf("O número em BCD é: ");
+
+
+    int potenciaDeDez = 1;
+    while (numero / potenciaDeDez > 0) {
+        potenciaDeDez *= 10;
+    }
+    potenciaDeDez /= 10;
+
+    while (potenciaDeDez > 0) {
+        digito = numero / potenciaDeDez; 
+        numero = numero % potenciaDeDez; 
+        potenciaDeDez /= 10;
+
+
+        switch(digito) {
+            case 0: printf("0000 "); break;
+            case 1: printf("0001 "); break;
+            case 2: printf("0010 "); break;
+            case 3: printf("0011 "); break;
+            case 4: printf("0100 "); break;
+            case 5: printf("0101 "); break;
+            case 6: printf("0110 "); break;
+            case 7: printf("0111 "); break;
+            case 8: printf("1000 "); break;
+            case 9: printf("1001 "); break;
+        }
+    }
+    printf("\n");
+}
+
+
 int main(){
 
     printf("Bem-vindo a calculadora didatica! primeiramente insira o valor a ser convertido.\n");
     scanf("%d", &valorInput);
-    converterBinario(valorInput);
+    converterParaBinario(valorInput);
+    converterHexa(valorInput);
+    converterOcta(valorInput);
+    converterParaBCD(valorInput);
+
 
     return 0;
 }
